1434929d222f316cbdc7d993979bf80c11e17d77
carloscels <carlos.cels.1999@gmail.com>
2020-07-08 16:01:10 -0300

    tabuleiro

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,14 @@
 package aplication;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		 System.out.println("hello word");
+		
+		ChessMatch chessMatch = new ChessMatch(); 
+		UI.printBoard(chessMatch.getPieces());	
+		
 	}
 	
 }
